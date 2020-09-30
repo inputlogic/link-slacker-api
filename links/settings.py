4179fb14bed92379a494bfc4c63ff3ebbe91c621
Gavin Vickery <gavin@geekforbrains.com>
2020-09-30 15:20:50 -0700

    Updating settings to use statics. Adding statics dir.

--- links/settings.py
+++ links/settings.py
@@ -135,7 +135,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'statics'),
+]
 
 # Slack app values
 # https://api.slack.com/apps/{APP_ID}/oauth?
