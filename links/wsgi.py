2374e7c294d6cc3e996a22091b705a2d04fe2e3a
Shawn Adrian <shawn@nerdburn.com>
2020-10-07 10:25:49 -0700

    updating wsgi

--- links/wsgi.py
+++ links/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', "<PROJ_NAME>.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'links.settings')
 
 application = get_wsgi_application()
